5812260320e3e6d15ddfbfdd778f93b423b51a54
Khaldir <jaketrigg@gmail.com>
2016-05-02 00:14:26 +0100

    Select Campaign form version 1

--- gmTool/Form1.Designer.cs
+++ gmTool/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.browseButton = new System.Windows.Forms.Button();
+            this.filePathDisplay = new System.Windows.Forms.TextBox();
+            this.openCampaignButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(266, 9);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 0;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // filePathDisplay
+            // 
+            this.filePathDisplay.Location = new System.Drawing.Point(12, 12);
+            this.filePathDisplay.Name = "filePathDisplay";
+            this.filePathDisplay.Size = new System.Drawing.Size(248, 20);
+            this.filePathDisplay.TabIndex = 1;
+            // 
+            // openCampaignButton
+            // 
+            this.openCampaignButton.Enabled = false;
+            this.openCampaignButton.Location = new System.Drawing.Point(12, 244);
+            this.openCampaignButton.Name = "openCampaignButton";
+            this.openCampaignButton.Size = new System.Drawing.Size(329, 23);
+            this.openCampaignButton.TabIndex = 0;
+            this.openCampaignButton.Text = "Open Campaign";
+            this.openCampaignButton.UseVisualStyleBackColor = true;
+            this.openCampaignButton.Click += new System.EventHandler(this.openCampaignButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Campaign Files (.cpn)|*.cpn";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(329, 200);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(317, 175);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(353, 275);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.openCampaignButton);
+            this.Controls.Add(this.filePathDisplay);
+            this.Controls.Add(this.browseButton);
+            this.Name = "Form1";
+            this.Text = "Select Campaign";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox filePathDisplay;
+        private System.Windows.Forms.Button openCampaignButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- gmTool/Form1.cs
+++ gmTool/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace gmTool
 {
@@ -15,6 +16,33 @@ namespace gmTool
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void folderBrowserDialog_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            groupBox1.Text = openFileDialog1.SafeFileName.Substring(0, openFileDialog1.SafeFileName.Length - 4);
+            using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+            {
+                // Read the stream to a string, and write the string to the console.
+                string line = sr.ReadToEnd();
+                textBox1.Text = line;
+            }
+            openCampaignButton.Enabled = true;
+        }
+
+        private void openCampaignButton_Click(object sender, EventArgs e)
+        {
+            //Open main form, pass filepath
         }
     }
 }
